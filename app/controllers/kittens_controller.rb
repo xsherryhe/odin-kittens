c9b2c640f0d7d1771a9d03dded9ae997baa728ca
Sherry He <xsherryhe@gmail.com>
2022-08-21 21:36:41 -0400

    Added links and flash messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
-      redirect_to @kitten
+      redirect_to @kitten, notice: 'Successfully created kitten.'
     else
+      flash.now[:error] = 'You messed up! Check the form again.'
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,8 +30,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
-      redirect_to @kitten
+      redirect_to @kitten, notice: 'Successfully updated kitten.'
     else
+      flash.now[:error] = 'You messed up! Check the form again.'
       render :edit, status: :unprocessable_entity
     end
   end
@@ -38,7 +40,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
-    redirect_to kittens_path
+    redirect_to kittens_path, notice: 'Successfully deleted kitten.'
   end
 
   private
